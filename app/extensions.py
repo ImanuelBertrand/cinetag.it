869c67bdd459f88ebb469a7fed90c133c5c729cd
Imanuel Bertrand <imanuel@bertrand.one>
2024-07-04 22:36:39 +0200

    MySQL support

--- app/extensions.py
+++ app/extensions.py
@@ -8,6 +8,9 @@ from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_apscheduler import APScheduler
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Initialize extensions
 db = SQLAlchemy()
