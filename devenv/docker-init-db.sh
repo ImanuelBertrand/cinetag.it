0ac00adbf05fea6b313562b95caf67c0150e0e77
Imanuel Bertrand <imanuel@bertrand.one>
2026-02-20 11:16:35 +0200

    remove devenv

--- devenv/docker-init-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Run the database migration script inside the Docker container
-echo "Running database migrations..."
-echo "This will create or update the database schema based on the latest migrations."
-cd $(dirname "$0") && docker compose -p cinetagit exec app python /app/devenv/docker-init-db.py
-
-echo "Database migrations complete."
-echo "If you need to create new migrations, use: flask db migrate -m 'Description of changes'"
--- devenv/docker-stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Stop the Docker containers
-echo "Stopping Docker containers..."
-cd $(dirname "$0")
-docker compose -p cinetagit down
-
-echo "Docker containers have been stopped."
