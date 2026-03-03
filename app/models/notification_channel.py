7204e1af8efbdc9770076e4c4f83c7a189b2da42
Imanuel Bertrand <imanuel@bertrand.one>
2026-03-03 12:42:50 +0200

    Fix db enum name for alembic

--- app/models/notification_channel.py
+++ app/models/notification_channel.py
@@ -10,7 +10,9 @@ class NotificationChannel(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     enabled = db.Column(db.Integer)
     days_in_advance = db.Column(db.JSON, nullable=False)
-    mode = db.Column(db.Enum("email", "push"), nullable=False)
+    mode = db.Column(
+        db.Enum("email", "push", name="user_notification_mode"), nullable=False
+    )
     notification_data = db.Column(db.JSON, nullable=True)
     include_maybe_movies = db.Column(db.Boolean, default=True)
     created_at = db.Column(db.DateTime, default=datetime.now(UTC))
